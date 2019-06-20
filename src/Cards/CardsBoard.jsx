7d7cc8dbab48d7d6f49b1d8e0b721e0d595177c6
Jorge Treviño Soteras <jorge@MacBook-Pro-de-Oramon.local>
2019-06-20 16:48:09 +0200

    filter broken

--- src/Cards/CardsBoard.jsx
+++ src/Cards/CardsBoard.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Card from './Card';
 import initialDeckJson from '../resources/cards';
+import { filter } from 'rsvp';
 
 import('./cards-board.css');
 
@@ -10,9 +11,10 @@ class CardsBoard extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { value: '' };
+    this.state = { deck: [] };
 
     this.getDefaultDeck = this.getDefaultDeck.bind(this);
+    this.filterCards = this.filterCards.bind(this);
   }
 
   getDefaultDeck(url) {
@@ -29,6 +31,16 @@ class CardsBoard extends React.Component {
 
   }
 
+  filterCards(event) {
+    const value = event && event.target.value || '';
+    console.log(value)
+    if (value.length >= 3) {
+      this.setState({ deck: this.props.currentDeck.find(card => card.name.includes(value)) });
+    } else {
+      this.setState({ deck: this.props.currentDeck });
+    }
+  }
+
   componentDidMount() {
     if (!this.props.currentDeck) {
       this.getDefaultDeck('https://swapi.co/api/people/1/');
@@ -37,10 +49,19 @@ class CardsBoard extends React.Component {
 
   render() {
     const { currentDeck, removeCard } = this.props;
+    let { deck } = this.state;
+    console.log(deck)
     return (
-      <div className="cards-board">
-        {currentDeck && currentDeck.map(cardInfo => <Card name={cardInfo.name} removeCard={() => removeCard(cardInfo._id)} key={cardInfo._id} />)}
-      </div>
+      <React.Fragment>
+        <input type="text" onChange={this.filterCards} />
+        <div className="cards-board">
+          {deck ? deck.map(
+            cardInfo => <Card name={cardInfo.name} removeCard={() => removeCard(cardInfo._id)} key={cardInfo._id} />
+          ) : currentDeck.map(
+            cardInfo => <Card name={cardInfo.name} removeCard={() => removeCard(cardInfo._id)} key={cardInfo._id} />
+          )}
+        </div>
+      </React.Fragment>
     )
   }
 }
